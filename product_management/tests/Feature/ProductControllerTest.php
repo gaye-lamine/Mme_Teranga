72e293b9c7c2ca2a375b882f24b0e798979c8c4d
abda-gaye <lamineg049@gmail.com>
2024-02-15 11:46:09 +0000

    implemention backend with laravel of the test for integrate madame teranga v2.1

--- product_management/tests/Feature/ProductControllerTest.php
+++ product_management/tests/Feature/ProductControllerTest.php
@@ -13,7 +13,7 @@ class ProductControllerTest extends TestCase
      * A basic feature test example.
      */
 
-    public function test_store_method_adds_new_product()
+    public function test_store_method_product()
     {
         $productData = [
             'title' => 'title 1',
@@ -36,7 +36,7 @@ class ProductControllerTest extends TestCase
         ]);
     }
 
-    public function test_index_method_get_all_products()
+    public function test_index_method_products()
     {
         $response = $this->getJson("api/product");
         $response->assertStatus(200)
@@ -47,7 +47,7 @@ class ProductControllerTest extends TestCase
             );
     }
 
-    public function test_show_method_show_product()
+    public function test_show_method_product()
 {
     $product = Product::create([
         'title' => 'title 1',
@@ -70,5 +70,50 @@ class ProductControllerTest extends TestCase
         'quantity' => 1
     ]);
 }
+
+public function test_delete_method_removes_product()
+{
+    $product = Product::create([
+        'title' => 'Produit à supprimer',
+        'description' => 'Description du produit à supprimer',
+        'price' => 1000,
+        'quantity' => 5
+    ]);
+
+    $response = $this->deleteJson('/api/product/'.$product->id);
+
+    $response->assertStatus(202)
+             ->assertJson([
+                 'status' => 'success'
+             ]);
+    $this->assertDatabaseMissing('products', [
+        'id' => $product->id
+    ]);
+}
+
+public function test_update_method_updates_product()
+{
+    $product = Product::create([
+        'title' => 'Produit à mettre à jour',
+        'description' => 'Description du produit à mettre à jour',
+        'price' => 1000,
+        'quantity' => 5
+    ]);
+
+    $newData = [
+        'title' => 'Nouveau titre',
+        'description' => 'Nouvelle description',
+        'price' => 1500,
+        'quantity' => 10
+    ];
+
+    $response = $this->patchJson('/api/product/' . $product->id, $newData);
+    $response->assertStatus(202)
+             ->assertJson([
+                 'status' => 'success'
+             ]);
+    $this->assertDatabaseHas('products', $newData);
+}
+
 
 }
